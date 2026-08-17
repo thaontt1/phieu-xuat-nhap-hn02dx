7ac91ad30adb46afdc6ec2d636b923989ea94530
Thao Nguyen <114843570+thaontt1@users.noreply.github.com>
2026-08-17 13:45:46 +0700

    Update constants.js

--- js/constants.js
+++ js/constants.js
@@ -49,7 +49,7 @@ var NHAN = {
     tinh: 'Tỉnh xuất',
     nguoi: 'Tên người xuất',
     phVeTinh: 'VD: Thái Bình',
-    phVeNguoi: 'Họ tên người xuất hàng'
+    phVeNguoi: 'Tên người xuất hàng'
   },
   nhap: {
     ten: 'PHIẾU NHẬP',
@@ -59,7 +59,7 @@ var NHAN = {
     tinh: 'Tỉnh nhập',
     nguoi: 'Tên người nhập',
     phVeTinh: 'VD: Hải Dương',
-    phVeNguoi: 'Họ tên người nhập hàng'
+    phVeNguoi: 'Tên người nhập hàng'
   }
 };
 
